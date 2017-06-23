086ffbd4a8b6dd64a8a7009ea80862f66a94f7ff
Marius Holstad <MariusHolstad@users.noreply.github.com>
2017-06-23 21:35:53 +0100

    Changed scene path

--- MindGamezJamchester2017/GameViewController.swift
+++ MindGamezJamchester2017/GameViewController.swift
@@ -16,7 +16,7 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "Scenes/TestScene01.scn")!
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
